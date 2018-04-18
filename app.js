de89f2ccbe1c70cc49523447fd4f0cbf3f94dc16
Maxwell Hunter <maxwell@treehousestickers.com>
2018-04-18 15:24:15 -0700

    added show and remove buttons

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const display = document.getElementById("counter");
 const headline = document.getElementById("headline");
+const inline = document.getElementById("inline");
 const textBox = document.getElementById("input");
 const list = document.getElementById("list");
 let inputValue = document.getElementById("input").value;
@@ -11,6 +12,7 @@ function reload() {
 	let body = document.getElementById("body");
 	let inputValue = document.getElementById("input").value;
 	headline.style.color="dark"+inputValue;
+	inline.style.backgroundColor="light"+inputValue;
 	document.bgColor=inputValue;
 	if (inputValue=="white"||inputValue=="yellow") {
 		document.getElementById("body").style.color="black";
@@ -18,8 +20,31 @@ function reload() {
 		document.getElementById("body").style.color="white";
 	}
 	counter+=1;
-	colorList=colorList+counter+". "+inputValue+"<br/>";
-	list.innerHTML=colorList;
+
+	// add the new color to a list item
+	let newListItem=document.createElement("li")
+	newListItem.textContent=inputValue;
+
+	// add a show button to the list item, to show that color again
+	let showButton=document.createElement("button");
+	showButton.textContent="show";
+	newListItem.appendChild(showButton);
+	showButton.addEventListener("click", () => {
+		textBox.value=inputValue;
+		reload();
+	});
+
+	// add a delete button to the list item
+	let delButton=document.createElement("button");
+	delButton.textContent="delete";
+	newListItem.appendChild(delButton);
+	delButton.addEventListener("click", () => {
+		list.removeChild(newListItem);
+	});
+
+
+	list.appendChild(newListItem);
+
 	display.textContent="You've tried "+counter+" colors so far.";
 	headline.textContent="Maxwell Hunter's favorite color is "+inputValue;
 }
